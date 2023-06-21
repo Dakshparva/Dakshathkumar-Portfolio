354c2a9ae9ff655f853a7461103ddb07ed7b9721
Dakshathkumar N <101206655+Dakshparva@users.noreply.github.com>
2023-06-21 22:32:28 +0530

    portfolio

---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Ecommerce-Amazonapp",
+        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+        tags:["JavaScript,HTML,CSS,React-js,React-redux,React-router,firebase,stripe"],
+        
+        demo:"https://amazon-2-arp.vercel.app/",
+        github:"https://github.com/Dakshparva/Amazon_App.git"
+    },
+    {
+        id:2,
+        name:"Youtube-Clone",
+        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
+        tags:["JavaScript,HTML,CSS,React-js,React-redux,React-router"],
+        
+        demo:"https://youtubeclone571.vercel.app/",
+        github:"https://github.com/Dakshparva/youtubeclone571.git"
+        
+    },
+    {
+        id:3,
+        name:"Movix website",
+        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
+        tags:["JavaScript,HTML,CSS,React-js,React-redux,React-router,api,firebase"],
+        
+        demo:"https://movix-eta.vercel.app/",
+        github:"https://github.com/Dakshparva/MovieFlixWeb.git"
+    },
+    {
+        id:4,
+        name:"Dcart-Ecommerce",
+        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+        tags:["JavaScript,HTML,CSS"],
+        
+        demo:"https://d-cart-dakshparva.vercel.app/",
+        github:"https://github.com/Dakshparva/D-Cart.git"
+       
+    },{
+        id:5,
+        name:"Todo App(React + Redux)",
+        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+        tags:["react","redux"],
+        
+        demo:"https://dakshparva.github.io/Todo-using-React/",
+        github:"https://github.com/Dakshparva/Todo-using-React"
+        
+    },
+    {
+        id:6,
+        name:"Eventmanagementapp",
+        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+        tags:["react","react-router","css"],
+        
+        demo:"https://dakshparva.github.io/eventmanagement1/",
+        github:"https://github.com/Dakshparva/eventmanagement1"
+        
+    },
+    
+  
+] 
